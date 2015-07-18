5a5869284e8a8d6a6eb3f70f42e97fe6f1c3607a
Pedram Veisi <pedramveisi@gmail.com>
2015-07-18 18:04:09 +0430

    Make Details View Functional
    Implement the details view using a CursorLoader, loading the data URI from the Intent. Sharing is changed accordingly.
    Make some methods public in Utility.java

--- app/src/main/java/si/vei/pedram/sunshine/activities/DetailActivity.java
+++ app/src/main/java/si/vei/pedram/sunshine/activities/DetailActivity.java
@@ -31,7 +31,6 @@ public class DetailActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
