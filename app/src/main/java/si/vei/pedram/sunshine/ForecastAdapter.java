d1d29ce7a31872dafc351ee457cbb1eea6546c1c
Pedram Veisi <pedramveisi@gmail.com>
2015-07-19 15:55:42 +0430

    Add ViewHolder class to ForecastAdapter.java

--- app/src/main/java/si/vei/pedram/sunshine/ForecastAdapter.java
+++ app/src/main/java/si/vei/pedram/sunshine/ForecastAdapter.java
@@ -93,4 +93,26 @@ public class ForecastAdapter extends CursorAdapter {
     public int getViewTypeCount() {
         return VIEW_TYPE_COUNT;
     }
+
+    /**
+     * Cache of the children views for a forecast list item.
+     */
+    public static class ViewHolder {
+        public final ImageView iconView;
+        public final TextView dateView;
+        public final TextView descriptionView;
+        public final TextView highTempView;
+        public final TextView lowTempView;
+
+        public ViewHolder(View view) {
+            iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+            dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+            descriptionView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+            highTempView = (TextView) view.findViewById(R.id.list_item_high_textview);
+            lowTempView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        }
+    }
+
+
 }
+
