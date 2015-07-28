f1d412db311289a1d17b5e6938a4f9de6a641a45
Pedram Veisi <pedramveisi@gmail.com>
2015-07-28 16:17:44 +0430

    Add a simple alarm to the app to start the service.

--- app/src/main/java/si/vei/pedram/sunshine/service/SunshineService.java
+++ app/src/main/java/si/vei/pedram/sunshine/service/SunshineService.java
@@ -1,8 +1,10 @@
 package si.vei.pedram.sunshine.service;
 
 import android.app.IntentService;
+import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -317,4 +319,14 @@ public class SunshineService extends IntentService {
         return locationId;
     }
 
+    public static class AlarmReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent sendIntent = new Intent(context, SunshineService.class);
+            sendIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, intent.getStringExtra(SunshineService.LOCATION_QUERY_EXTRA));
+            context.startService(sendIntent);
+
+        }
+    }
 }
