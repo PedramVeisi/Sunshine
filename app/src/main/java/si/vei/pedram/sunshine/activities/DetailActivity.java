3014a3b5db531195d7ead576b798d430f8cd3909
Pedram Veisi <pedramveisi@gmail.com>
2015-07-31 17:43:44 +0430

    Resolve a bug in Detail Activity
    Now, share action is shown just once.

--- app/src/main/java/si/vei/pedram/sunshine/activities/DetailActivity.java
+++ app/src/main/java/si/vei/pedram/sunshine/activities/DetailActivity.java
@@ -35,7 +35,7 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail_fragment, menu);
+        // getMenuInflater().inflate(R.menu.menu_detail_fragment, menu);
         return true;
     }
 
